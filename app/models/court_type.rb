7d06bf56793177b607dd331d3f92ee1f37bc5de9
Justin Koslitz <justin.koslitz@gmail.com>
2021-11-23 15:31:42 +0100

    added validations

--- app/models/court_type.rb
+++ app/models/court_type.rb
@@ -1,3 +1,5 @@
 class CourtType < ApplicationRecord
   has_many :courts
+
+  validates :name, presence: true
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,22 @@ class Lesson < ApplicationRecord
   belongs_to :user
   belongs_to :court
   has_many :reservations
+
+  validates :price, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :capacity, presence: true
+
+  validates :date_validation
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
+  def date_validation
+    if self[:end_time] < self[:start_time]
+      errors[:end_time] << "Please make sure that start date is before end date"
+      return false
+    else
+      return true
+    end
+  end
 end
