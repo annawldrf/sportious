8c497a18cd447b06dbedb941f30be56649546950
Justin Koslitz <justin.koslitz@gmail.com>
2021-11-24 14:39:40 +0100

    implemented initial map

--- app/controllers/courts_controller.rb
+++ app/controllers/courts_controller.rb
@@ -1,2 +1,13 @@
 class CourtsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+  def index
+    @courts = Court.all
+    @markers = @courts.geocoded.map do |court|
+      {
+        lat: court.latitude,
+        lng: court.longitude,
+        image_url: helpers.asset_url("icon.png")
+      }
+    end
+  end
 end
