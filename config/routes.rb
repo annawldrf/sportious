9c934c00e8ef4e5990660a6d8e0717e885d8592d
Justin Koslitz <justin.koslitz@gmail.com>
2021-11-29 09:56:11 +0100

    did some changes!

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :courts, :reviews, only: [:new, :create, :show, :index] do
-    resources :check_ins, :reviews, only: [:create]
+  resources :courts, only: [:new, :create, :show, :index] do
+    resources :check_ins, only: [:create]
+    resources :reviews, :lessons, only: [:new, :create, :show, :index]
   end
-  resources :lessons, only: [:new, :create, :show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
