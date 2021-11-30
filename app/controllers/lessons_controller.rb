5861b2df80cb0c29c07350db786a4a573f5c10ee
Anna Waldorf <anna@waldorf.de>
2021-11-30 11:41:44 +0100

    index lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,27 @@
 class LessonsController < ApplicationController
+
+  def index
+    @lessons = Court.find(params[:court_id]).lessons
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+
+  def create
+    @lesson = Lesson.new(strong_params)
+    @lesson.user = current_user
+    if @lesson.save
+      redirect_to lesson_path(@lesson)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:lesson).permit(:title, :description, :start_time, :end_time, :capacity, :price, :user_id, :court_id)
+  end
+
 end
