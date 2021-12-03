ba0c556a26c77a2397e3c0c38e172cbc48451de4
Anna Waldorf <anna@waldorf.de>
2021-12-03 11:16:24 +0100

    Heroku fix to flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,20 +24,3 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   initStarRating();
 });
-
-// ./packs/application.js
-//import { Application } from 'stimulus'
-//import { definitionsFromContext } from 'stimulus/webpack-helpers'
-
-//const application = Application.start()
-//const context = require.context('../controllers', true, /\.js$/)
-//application.load(definitionsFromContext(context))
-
-// import Flatpickr
-import Flatpickr from 'stimulus-flatpickr'
-
-// Import style for flatpickr
-require("flatpickr/dist/flatpickr.css")
-
-// Manually register Flatpickr as a stimulus controller
-application.register('flatpickr', Flatpickr)
